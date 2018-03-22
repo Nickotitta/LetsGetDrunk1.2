70a1e3e2bafd37f02bf93c9641339a0dea5442c8
Nick <nick_frej@hotmail.com>
2018-03-22 22:04:02 +0100

    Added a new function to Talk side

--- app/src/main/java/com/example/nick/letsgetdrunk12/gamesWithTalks.java
+++ app/src/main/java/com/example/nick/letsgetdrunk12/gamesWithTalks.java
@@ -1,15 +1,28 @@
 package com.example.nick.letsgetdrunk12;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class gamesWithTalks extends AppCompatActivity {
 
+    private ImageButton iveNever;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_games_with_talks);
 
         // Create a I´ve never fuction here
+        iveNever = (ImageButton)findViewById(R.id.iveNeverButton);
+        iveNever.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent go = new Intent(gamesWithTalks.this, NeverTheGame.class);
+                startActivity(go);
+            }
+
+        });
     }
 }
