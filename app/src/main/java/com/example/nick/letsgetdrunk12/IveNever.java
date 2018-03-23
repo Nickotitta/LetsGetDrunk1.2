230f275dd30c5075d80f5983ef65062ec7644c96
Nick Frejneman <nick.frejneman@gmail.com>
2018-03-23 14:45:24 +0100

    fixed the listView and added a create list page

--- app/src/main/java/com/example/nick/letsgetdrunk12/IveNever.java
+++ app/src/main/java/com/example/nick/letsgetdrunk12/IveNever.java
@@ -1,14 +1,18 @@
 package com.example.nick.letsgetdrunk12;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class IveNever extends AppCompatActivity {
 
     ListView listView;
+    private Button createAList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +22,27 @@ public class IveNever extends AppCompatActivity {
 
         listView = (ListView)findViewById(R.id.ListViewIveNever);
 
+        ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(IveNever.this, android.R.layout.simple_list_item_1,
+        getResources().getStringArray(R.array.talkLists));
+
+        createAList = (Button)findViewById(R.id.createNewListBtn);
+        createAList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent create = new Intent (IveNever.this, CreateAList.class);
+                startActivity(create);
+            }
+        });
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(IveNever.this, NeverTheGame.class);
+                intent.putExtra("ListOfTheNeverGame", listView.getItemAtPosition(position).toString());
+                startActivity(intent);
 
             }
         });
+        listView.setAdapter(mAdapter);
     }
 }
