fcc558660d951dbf76973e699bf469ca9281a342
Nick <nick_frej@hotmail.com>
2018-03-28 23:01:45 +0200

    Started with impelenting Webview

---
+++ app/src/main/java/com/example/nick/letsgetdrunk12/RingOfFireWeb.java
@@ -0,0 +1,13 @@
+package com.example.nick.letsgetdrunk12;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RingOfFireWeb extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ring_of_fire_web);
+    }
+}
